bf927bf8b14136f33847dff23a94e42b80b2a43d
Christopher Flores <Christopher321flores@gmail.com>
2026-04-04 16:39:51 -0400

    first set of layout done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./components/navbar";
-import homeStyle from '../CSS/home.module.css'
-import introStyle from '../CSS/intro.module.css'
+import homeStyle from '../CSS/home.module.css';
+import introStyle from '../CSS/intro.module.css';
+import skillsetStyle from '../CSS/skillset.module.css';
+import workexperienceStyle from '../CSS/workexperience.module.css';
+
+
 function Home (){
     return (
-        <>
+        <>  
+        
             <div className={homeStyle.start}>
                 <Navbar/>
                 <div className={introStyle.intro}>
@@ -17,10 +22,33 @@ function Home (){
                     </div>
                 </div>
             </div>
-            <div className="skillset">
+            <div className={skillsetStyle.skillSet}>
                 <div className="Title">Technical Skills</div>
+                    {/* All to be replaced ones we know how to implement lists */}
+
                 <div className="list">
-                        
+                    List of programming languages
+                </div>
+            </div>
+            <div className={workexperienceStyle.work_experience}>
+                <div className={workexperienceStyle.workimage}>
+                    <img src="" alt="" className={workexperienceStyle.workimg}/>
+                </div>
+                <div className={workexperienceStyle.work_details}>
+                    {/* All to be replaced ones we know how to implement lists */}
+                    <div className={workexperienceStyle.title}>Title</div>
+                    <div className={workexperienceStyle.timeline}>{`Start - End`}</div>
+                    <div className={workexperienceStyle.responsibility}>
+                        <div className={workexperienceStyle.details}>
+                            <ul className={workexperienceStyle.list}>
+                                <li className={workexperienceStyle.experience}>item1</li>
+                                <li className={workexperienceStyle.experience}>item2</li>
+                                <li className={workexperienceStyle.experience}>item3</li>
+                                <li className={workexperienceStyle.experience}>item4</li>
+                                <li className={workexperienceStyle.experience}>item5</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
