5c7fc3f4a975bde336ea701db5eafcc3019ee0ff
Christopher Flores <Christopher321flores@gmail.com>
2026-04-05 19:02:05 -0400

    adding list for work experience

---
+++ src/pages/components/experienceList.jsx
@@ -0,0 +1,6 @@
+export const work_list = [
+    {id: 1, img : '', title: 'Sales Associate', employeer: 'Uniqlo', startDate: '10/2026', endDate: 'Present',discription: []},
+    {id: 2, img : '', title: 'Software Developer Intern', employeer: 'Our National Conversation', startDate: '', endDate: '',discription: []},
+    {id: 3, img : '', title: 'Assistant Manager', employeer: 'Nangman BBQ', startDate: '', endDate: '',discription: []},
+    {id: 4, img : '', title: 'Server/Barista', employeer: 'Cafe Beane', startDate: '', endDate: '',discription: []}
+];
