50bb926fd2c3ddb19facadf1bc8d6719ad25e43e
Christopher Flores <Christopher321flores@gmail.com>
2026-04-09 23:19:25 -0400

    prepare for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // vite.config.js
-  base: "/",
   plugins: [react()],
-  server: {
-    host: true
-  },
+
 })
