d875872498bc9a4312dcf78a713983bb7702045e
Christopher Flores <Christopher321flores@gmail.com>
2026-04-09 22:12:38 -0400

    edits

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // vite.config.js
+  base: "/ChristopherFlores/",
   plugins: [react()],
   server: {
     host: true
